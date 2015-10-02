db2fb0fa4266f1c89f03accd098805c7ad6d4178
towusu <taowusu@gmail.com>
2015-10-02 16:33:49 -0400

    Lab 1 completed.

--- lab1.py
+++ lab1.py
@@ -17,4 +17,11 @@ def vowel_or_consonant():
     "sometimes a vowel, sometimes a consonant". Otherwise your program should
     display a message indicating that the letter is a "consonant".
     """
+    letter = (raw_input("Enter a Letter"))
 
+    if letter in "aeiou":
+        print ("vowel")
+    elif letter in "y":
+        print ("sometimes a vowel, sometimes a consonant")
+    else:
+        print ("consonant")
