dfdc865cbf6e3781e020f20734e67b2b4c263e91
owusuthe <therese.owusu@mail.utoronto.ca>
2015-10-29 11:11:34 -0400

    Test_lab3 updates to last two tests

--- test_lab3.py
+++ test_lab3.py
@@ -12,6 +12,7 @@ from lab3 import days_in_month
 MONTHS_WITH_31 = ["January", "March", "May", "July", "August", "October", "December"]
 MONTHS_WITH_30 = ["April", "June", "September", "November"]
 MONTHS_WITH_28_or_29 = ["February"]
+MONTHS_WITH_CAPITALIZED = MONTHS_WITH_31 + MONTHS_WITH_30 + MONTHS_WITH_28_or_29
 
 
 def test_months_with_31():
@@ -22,15 +23,30 @@ def test_months_with_31():
         assert days_in_month(item) == 31
 
 # Write a test function for the months with 30 days
+def test_months_with_30():
 
 
 # Write a test function for the months with 28 or 29 days
-
+    for item in MONTHS_WITH_30:
+        assert days_in_month(item) == 30
 
 # Write a test function for months that are not capitalized properly
+def test_months_with_capitalized():
 # Hint: use the lower() string method
-
+    try:
+        month = month.capitalize()
+    except ValueError:
+        assert True
 # Write a test function for unexpected input
+#def test_function_for_unexpected_input
 # Hint: use a try/except block to deal with the exception
+"""
+    try:
+        some function call
+    except SomeError:
+        We get here, because an exception was raised
+        assert True
+
+"""
 # Hint: use data types other than strings as input
 
